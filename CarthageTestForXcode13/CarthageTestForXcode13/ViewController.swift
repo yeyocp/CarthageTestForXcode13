fe9353a2c76891af4f6d80cebdc8cb00b97fcc44
Sergio Castaño Panchuelo <>
2021-09-29 10:11:46 +0200

    Added CarthageTestForXcode13 project.

---
+++ CarthageTestForXcode13/CarthageTestForXcode13/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CarthageTestForXcode13
+//
+//  Created by Sergio Castaño Panchuelo on 29/9/21.
+//
+
+import UIKit
+import RxSwift
+import Charts
+
+import IQKeyboardManager
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
